e4675b3bf05116ea46281c90c00e62cc954a14f1
kevin <segaud.kevin@gmail.com>
2019-08-04 19:22:39 +0200

    fix fail caused by no main

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 // import 'package:stashall/main.dart';
 
-// void main() {
+void main() {
 //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 //     // Build our app and trigger a frame.
 //     await tester.pumpWidget(MyApp());
@@ -27,4 +27,4 @@ import 'package:flutter_test/flutter_test.dart';
 //     expect(find.text('0'), findsNothing);
 //     expect(find.text('1'), findsOneWidget);
 //   });
-// }
+}
